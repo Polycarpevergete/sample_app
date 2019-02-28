068e4bec262e824771749dcc84a1b8a13fd3236f
polycarpevergete <xavier.sassi@gmail.com>
2019-02-28 12:30:34 +0700

    Solved error on keys for AWS/Heroku

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
     # Configuration for Amazon S3
         :provider               => 'AWS',
-        :aws_access_key_id      => ENV['AKIAIY27LQQZESTPUPCA'],
-        :aws_secret_key_id      => ENV['wqR//+6pm8uJGdi0HUclroVVO10avnEN8q9Ko9RV']
-    }
+        :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
+        :aws_secret_key_id      => ENV['S3_SECRET_KEY']
+    }   
     config.fog_directory        = ENV['S3_BUCKET']
     config.fog_provider         = 'fog/aws'
   end
